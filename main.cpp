34ee541a4ccc41430d27f0d42062a8862833d2c3
礒邉大輝 <k022g1059@g.neec.ac.jp>
2023-09-05 13:05:31 +0900

    9/5 シーン切り替え、画像読み込み

--- main.cpp
+++ main.cpp
@@ -6,14 +6,34 @@ const char kWindowTitle[] = "10days";
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
+	Novice::Initialize(kWindowTitle, 800, 640);
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
-	enum Scene { Title, setumei, game, clear };
-	int scene = Scene::Title;
+	//マウスカーソル非表示とフルスクリーン化(事故防止のため最後に有効化)
+	//ShowCursor(FALSE);
+	//Novice::SetWindowMode(kFullscreen);
+
+
+	//シーン
+	enum Scene { title, setumei, game, clear };
+	int scene = Scene::title;
+
+	//クリアフラグ
+	int GameClearflag = 0;
+
+	//画面読み込み↓
+	
+	//背景画像
+	int Haikei = Novice::LoadTexture("./image/GameHaikei.png");
+
+	//ゴールエフェクト画像
+	int GoalEfect1 = Novice::LoadTexture("./image/GoalEfect1.png");
+	int GoalEfect2 = Novice::LoadTexture("./image/GoalEfect2.png");
+	int GoalEfect3 = Novice::LoadTexture("./image/GoalEfect3.png");
+
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -24,23 +44,66 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		memcpy(preKeys, keys, 256);
 		Novice::GetHitKeyStateAll(keys);
 
-		switch (scene){
-		case Title:
-			break;
-
-		case setumei:
-			break;
-
-		case game:
-			break;
-
-		case clear:
-			break;
-		}
-
 		///
 		/// ↓更新処理ここから
 		///
+
+		switch (scene){
+		case title:
+
+			//タイトル画像
+			Novice::DrawBox(0, 0, 800, 640, 0.0f, RED, kFillModeSolid);
+
+			//シーン切り替え
+			if (keys[DIK_SPACE] != 0 && preKeys[DIK_SPACE] == 0) {
+				scene = setumei;
+			}
+
+			break;
+
+		case setumei:
+
+			//説明画像
+			Novice::DrawBox(0, 0, 800, 640, 0.0f, BLUE, kFillModeSolid);
+
+			//シーン切り替え
+			if (keys[DIK_SPACE] != 0 && preKeys[DIK_SPACE] == 0) {
+				scene = game;
+			}
+
+			break;
+
+		case game:
+
+			//背景画像
+			Novice::DrawSprite(0, 0, Haikei, 1, 1, 0.0f, WHITE);
+
+			//ゴールエフェクト(エラー対策の仮置き)
+			Novice::DrawSprite(0, 0, GoalEfect1, 1, 1, 0.0f, WHITE);
+			Novice::DrawSprite(32, 0, GoalEfect2, 1, 1, 0.0f, WHITE);
+			Novice::DrawSprite(64, 0, GoalEfect3, 1, 1, 0.0f, WHITE);
+			
+			//シーン切り替え
+		    if(GameClearflag == 1){
+		    	scene = clear;
+		    }
+
+			break;
+
+		case clear:
+
+			//クリア画像
+			Novice::DrawBox(0, 0, 800, 640, 0.0f, GREEN, kFillModeSolid);
+
+			//シーン切り替え
+			if (keys[DIK_SPACE] != 0 && preKeys[DIK_SPACE] == 0) {
+				scene = title;
+				GameClearflag = 0;
+			}
+
+			break;
+		}
+
 		
 		///
 		/// ↑更新処理ここまで
